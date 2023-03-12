0a9b3057636cfd41ba38e808d741f97f1b14cba6
corey duke <cdukedev@gmail.com>
2023-03-12 12:29:59 -0400

    added react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
