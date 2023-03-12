18e22276d3b08bb79c03317385b605f84ae730f9
corey duke <cdukedev@gmail.com>
2023-03-12 17:58:45 -0400

    update router dom back to 5.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
