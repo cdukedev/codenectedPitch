aa3b4b5494623c955f42c9b6d90bda2018b8e166
corey duke <cdukedev@gmail.com>
2023-04-24 12:40:53 -0400

    Adjusted slide two responsivness

--- src/components/Slides/SlideTwo/SlideTwo.jsx
+++ src/components/Slides/SlideTwo/SlideTwo.jsx
@@ -21,7 +21,6 @@ const SlideTwo = () => {
 
   const cards = {
     maxWidth: 345,
-    margin: "0 auto",
     margin: "1rem 1rem",
     gap: "1rem",
     width: "40%",
@@ -32,99 +31,107 @@ const SlideTwo = () => {
   return (
     <main className="main__container">
       {/* if problem is not equal to main, then add the button above the cards */}
-      <section className="main__left">
-        {problem !== "Main" ? (
-          <Button
-            sx={{
-              margin: ".2rem 1rem",
-              backgroundColor: "rgba(255, 255, 255, 0.45)",
-              backdropFilter: "blur (14px)",
-              color: "white",
-            }}
-            className="main__button"
-            onClick={() => setProblem("Main")}
-          >
-            Back
-          </Button>
-        ) : null}
-        <Box
+      {problem !== "Main" ? (
+        <Button
           sx={{
-            width: "100%",
-            flexGrow: 0.5,
-            alignItems: "center",
-            justifyContent: "center",
+            margin: ".2rem 1rem",
+            backgroundColor: "rgba(255, 255, 255, 0.45)",
+            backdropFilter: "blur (14px)",
+            color: "white",
           }}
+          className="main__button"
+          onClick={() => setProblem("Main")}
         >
-          {/* if problem is equal to main, then add the button above the cards */}
-          <Grid
-            container
-            justifyContent="center"
-            rowSpacing={1}
-            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-          >
-            <CardActionArea
-              sx={cards}
-              onClick={() => setProblem("Non-Profits")}
-            >
-              <Card
-                className="main__card"
-                title="Non-Profits"
-                description="Local Businesses in need of technology to help them achieve their goals"
-                image={nonProfits}
-                imageTitle="design steps"
-              />
-            </CardActionArea>
-            <CardActionArea sx={cards} onClick={() => setProblem("Students")}>
-              <Card
-                className="main__card"
-                title="Students"
-                description="Local Businesses in need of technology to help them achieve their goals"
-                image={students}
-                imageTitle="design steps"
-              />
-            </CardActionArea>
-            <CardActionArea sx={cards} onClick={() => setProblem("Bootcamps")}>
-              <Card
-                className="main__card"
-                title="Bootcamps"
-                description="Local Businesses in need of technology to help them achieve their goals"
-                image={bootcamps}
-                imageTitle="design steps"
-              />
-            </CardActionArea>
-            <CardActionArea
-              sx={cards}
-              onClick={() => setProblem("Small-Businesses")}
-            >
-              <Card
-                className="main__card"
-                title="Small Businesses"
-                description="Local Businesses in need of technology to help them achieve their goals"
-                image={smallBusinesses}
-                imageTitle="design steps"
-              />
-            </CardActionArea>
-          </Grid>
-        </Box>
-      </section>
-
+          Back
+        </Button>
+      ) : null}
       {problem === "Main" ? (
-        <section className="main__right-main">
-          <p className="main__right-main-text">
-            The technology industry is constantly evolving and growing, but not
-            everyone has equal access to the resources and opportunities to
-            participate in this growth. This disparity affects not only
-            individuals but also small businesses and non-profit organizations
-            in under-served communities, who struggle to keep up with the rapid
-            pace of technological advancements. Meanwhile, students interested
-            in pursuing a career in technology face a number of challenges,
-            including a lack of experience in real-world tech projects and
-            difficulty finding meaningful opportunities to gain experience.
-            This, in turn, affects their ability to build a desirable resume and
-            to make a positive impact in the world.
-          </p>
-        </section>
-      ) : problem === "Non-Profits" ? (
+        <>
+          <section className="main__left">
+            <Box
+              sx={{
+                width: "100%",
+                flexGrow: 0.5,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              {/* if problem is equal to main, then add the button above the cards */}
+              <Grid
+                container
+                justifyContent="center"
+                rowSpacing={1}
+                columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+              >
+                <CardActionArea
+                  sx={cards}
+                  onClick={() => setProblem("Non-Profits")}
+                >
+                  <Card
+                    className="main__card"
+                    title="Non-Profits"
+                    description="Local Businesses in need of technology to help them achieve their goals"
+                    image={nonProfits}
+                    imageTitle="design steps"
+                  />
+                </CardActionArea>
+                <CardActionArea
+                  sx={cards}
+                  onClick={() => setProblem("Students")}
+                >
+                  <Card
+                    className="main__card"
+                    title="Students"
+                    description="Local Businesses in need of technology to help them achieve their goals"
+                    image={students}
+                    imageTitle="design steps"
+                  />
+                </CardActionArea>
+                <CardActionArea
+                  sx={cards}
+                  onClick={() => setProblem("Bootcamps")}
+                >
+                  <Card
+                    className="main__card"
+                    title="Bootcamps"
+                    description="Local Businesses in need of technology to help them achieve their goals"
+                    image={bootcamps}
+                    imageTitle="design steps"
+                  />
+                </CardActionArea>
+                <CardActionArea
+                  sx={cards}
+                  onClick={() => setProblem("Small-Businesses")}
+                >
+                  <Card
+                    className="main__card"
+                    title="Small Businesses"
+                    description="Local Businesses in need of technology to help them achieve their goals"
+                    image={smallBusinesses}
+                    imageTitle="design steps"
+                  />
+                </CardActionArea>
+              </Grid>
+            </Box>
+          </section>
+          <section className="main__right-main">
+            <p className="main__right-main-text">
+              The technology industry is constantly evolving and growing, but
+              not everyone has equal access to the resources and opportunities
+              to participate in this growth. This disparity affects not only
+              individuals but also small businesses and non-profit organizations
+              in under-served communities, who struggle to keep up with the
+              rapid pace of technological advancements. Meanwhile, students
+              interested in pursuing a career in technology face a number of
+              challenges, including a lack of experience in real-world tech
+              projects and difficulty finding meaningful opportunities to gain
+              experience. This, in turn, affects their ability to build a
+              desirable resume and to make a positive impact in the world.
+            </p>
+          </section>
+        </>
+      ) : null}
+      {problem === "Non-Profits" ? (
         <section className="main__right">
           <NonProfit />
         </section>
@@ -136,27 +143,7 @@ const SlideTwo = () => {
         <section className="main__right">
           <Bootcamp />
         </section>
-      ) : problem === "Small-Businesses" ? (
-        <section className="main__right">
-          <p>Small Businesses Problems</p>
-        </section>
-      ) : (
-        <section className="main__right-main">
-          <p className="main__right-main-text">
-            The technology industry is constantly evolving and growing, but not
-            everyone has equal access to the resources and opportunities to
-            participate in this growth. This disparity affects not only
-            individuals but also small businesses and non-profit organizations
-            in under-served communities, who struggle to keep up with the rapid
-            pace of technological advancements. Meanwhile, students interested
-            in pursuing a career in technology face a number of challenges,
-            including a lack of experience in real-world tech projects and
-            difficulty finding meaningful opportunities to gain experience.
-            This, in turn, affects their ability to build a desirable resume and
-            to make a positive impact in the world.
-          </p>
-        </section>
-      )}
+      ) : null}
     </main>
   );
 };
