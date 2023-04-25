58a3cc0010bae2788a8506a63e287d2edcdad938
corey duke <cdukedev@gmail.com>
2023-04-24 20:41:41 -0400

    Added content and styling to the what we do slide

--- src/components/Slides/SlideThree/SlideThree.jsx
+++ src/components/Slides/SlideThree/SlideThree.jsx
@@ -2,7 +2,43 @@ import React from "react";
 import "./SlideThree.scss";
 
 const SlideThree = () => {
-  return <section className="slide-three"></section>;
+  return (
+    <section className="container">
+      <div className="top">
+        <p className="top__text">
+          <strong>CodeNected</strong> is a platform that bridges the gap between
+          non-profit organizations, students, skilled volunteers, and
+          corporations, providing solutions to the following challenges:
+        </p>
+        <ol className="top__text-items">
+          <li className="top__text-item">
+            Limited resources and technology access for non-profits.
+          </li>
+          <li className="top__text-item">
+            Opportunities for students and volunteers to develop skills and
+            contribute to meaningful projects.
+          </li>
+          <li className="top__text-item">
+            Addressing corporations' talent pipeline, upskilling, and CSR
+            objectives.
+          </li>
+        </ol>
+      </div>
+      <div className="top">
+        <p className="top__text">
+          At CodeNected, we provide a platform that bridges the gap between
+          non-profit organizations, students, skilled volunteers, and
+          corporations. We streamline the onboarding process for non-profits,
+          identify their specific needs and challenges, and connect them with
+          students and skilled volunteers eager to contribute their expertise.
+          In partnership with corporations, we offer upskilling opportunities
+          for employees, strengthening their talent pipeline, and advancing
+          their CSR initiatives. Our platform fosters collaboration, growth, and
+          positive change, enabling all stakeholders to benefit and thrive.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default SlideThree;
